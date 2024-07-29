3b7b78c56a737d086e24877e30ee1fc40ccdb49a
AbdifatahAbdirizaq700 <ccabdirasaaq800@gmail.com>
2024-07-29 12:17:09 -1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import Statistics from './components/Statistics';
+import ContactUs from './components/ContactUs';
 
-function App() {
+const App = () => {
   return (
-    <div>hello world</div>
-  )
-}
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
