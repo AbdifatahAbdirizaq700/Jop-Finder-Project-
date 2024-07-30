dff3afea506d2fddb8e935efe042a23601814d1c
AbdifatahAbdirizaq700 <ccabdirasaaq800@gmail.com>
2024-07-30 08:53:41 -1100

    update

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,18 +1,28 @@
 // src/components/SearchBar.jsx
-import React from 'react';
-import { FaSearch } from 'react-icons/fa';
+import React, { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
   return (
     <div className="flex justify-center mb-4">
-      <div className="relative w-full max-w-lg">
-        <input
-          type="text"
-          className="border border-gray-300 rounded-lg p-2 pl-10 w-full hover:border-blue-600 transition-colors duration-300"
-          placeholder="Search for Job Title"
-        />
-        <FaSearch className="absolute top-2 right-3 text-gray-400 hover:text-blue-400 transition-colors duration-200" />
-      </div>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="border border-gray-300 rounded-lg p-2 w-full max-w-lg"
+        placeholder="Search for Job Title"
+      />
+      <button
+        onClick={handleSearch}
+        className="ml-2 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition-colors duration-300"
+      >
+        Search
+      </button>
     </div>
   );
 };
