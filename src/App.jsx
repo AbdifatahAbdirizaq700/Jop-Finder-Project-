163b1c984bb89da7b970b4a88fa98193a79724a8
AbdifatahAbdirizaq700 <ccabdirasaaq800@gmail.com>
2024-07-29 21:27:54 -1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import Statistics from './components/Statistics';
-import ContactUs from './pages/ContactUs';
-import HeroPage from './pages/HeroPage ';
 import Services from './components/Services';
 import News from './components/News';
-
+import HeroPage from './pages/HeroPage ';
 
 const App = () => {
   return (
@@ -17,12 +16,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/statistics" element={<Statistics />} />
-        <Route path="/Services" element={<Services />} />
-        <Route path="/News" element={<News />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/news" element={<News />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
-      
-      {/* <Footer /> */}
     </Router>
   );
 };
