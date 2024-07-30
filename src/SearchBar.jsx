5be5c03d6de739ad271b09c97a0e2674a598fc3f
AbdifatahAbdirizaq700 <ccabdirasaaq800@gmail.com>
2024-07-30 09:16:47 -1100

    update

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -4,6 +4,10 @@ import React, { useState } from 'react';
 const SearchBar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   const handleSearch = () => {
     onSearch(searchTerm);
   };
@@ -12,16 +16,16 @@ const SearchBar = ({ onSearch }) => {
     <div className="flex justify-center mb-4">
       <input
         type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
         className="border border-gray-300 rounded-lg p-2 w-full max-w-lg"
         placeholder="Search for Job Title"
+        value={searchTerm}
+        onChange={handleInputChange}
       />
       <button
         onClick={handleSearch}
-        className="ml-2 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition-colors duration-300"
+        className="ml-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors duration-300"
       >
-        Search
+        🔍
       </button>
     </div>
   );
