10145b75fbb7b30cc2b8eaeb2c37ca45b63ed1de
AbdifatahAbdirizaq700 <ccabdirasaaq800@gmail.com>
2024-07-30 04:57:07 -1100

    update

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,23 @@
 // src/components/HomePage.jsx
 import React from 'react';
-import SearchBar from '../SearchBar';
-import PopularSearches from '../PopularSearches ';
-import JobSidebar from '../JobSidebar';
-import JobList from '../JobList';
+import SearchBar from './SearchBar';
+import PopularSearches from './PopularSearches';
+import JobList from './JobList';
+import FilterSidebar from './FilterSidebar';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-4">Let's Find you the Perfect Job!</h1>
+      <p className="text-center mb-6">Learn ipsum dolor sit amet consectetur adipisicing elit. Harum excepturi debitis veniam.</p>
       <SearchBar />
       <PopularSearches />
-      <div className="flex">
-        <JobSidebar />
-        <JobList />
+
+      <div className="flex mt-6">
+        <FilterSidebar />
+        <div className="flex-1 ml-4">
+          <JobList />
+        </div>
       </div>
     </div>
   );
