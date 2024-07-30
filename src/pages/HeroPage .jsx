034fc2e30634cd2885aa3050871db7943e2fccbb
AbdifatahAbdirizaq700 <ccabdirasaaq800@gmail.com>
2024-07-30 08:15:26 -1100

    update

--- src/pages/HeroPage .jsx
+++ src/pages/HeroPage .jsx
@@ -5,11 +5,16 @@ const HeroPage = () => {
     <section className="text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+          <img className="object-cover object-center rounded" alt="hero" src="Images/pp.avif" />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Knausgaard typewriter readymade marfa</h1>
-          <p className="mb-8 leading-relaxed">Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid swag typewriter affogato, hella selvage wolf narwhal dreamcatcher.</p>
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+  Discover Your Dream Job
+</h1>
+<p className="mb-8 leading-relaxed">
+  Welcome to Job Finder, your ultimate destination to explore and apply for top job opportunities. Start your journey to a fulfilling career today!
+</p>
+
           <div className="flex w-full md:justify-start justify-center items-end">
             <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
               <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Placeholder</label>
