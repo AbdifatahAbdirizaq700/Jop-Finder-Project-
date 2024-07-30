f3e7a0a6aa5f6eed6c9e5a386e9f81e965486c9e
AbdifatahAbdirizaq700 <ccabdirasaaq800@gmail.com>
2024-07-29 20:48:24 -1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Header from './components/Header';
 import HomePage from './components/HomePage';
 import Statistics from './components/Statistics';
 import ContactUs from './pages/ContactUs';
+import HeroPage from './pages/HeroPage ';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <HeroPage />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
